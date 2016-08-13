1adaa15f54feb9d01d555967a1e1dd9d9f0af3ed
Nate Wright <natew@notthisway.com>
2016-08-13 16:43:34 +0100

    Remove unnecessary code with installation overrides

--- MakeSubmissionBlockPlugin.inc.php
+++ MakeSubmissionBlockPlugin.inc.php
@@ -19,50 +19,6 @@ import('lib.pkp.classes.plugins.BlockPlugin');
 
 class MakeSubmissionBlockPlugin extends BlockPlugin {
 	/**
-	 * Determine whether the plugin is enabled. Overrides parent so that
-	 * the plugin will be displayed during install.
-	 */
-	function getEnabled() {
-		if (!Config::getVar('general', 'installed')) return true;
-		return parent::getEnabled();
-	}
-
-	/**
-	 * Install default settings on system install.
-	 * @return string
-	 */
-	function getInstallSitePluginSettingsFile() {
-		return $this->getPluginPath() . '/settings.xml';
-	}
-
-	/**
-	 * Install default settings on journal creation.
-	 * @return string
-	 */
-	function getContextSpecificPluginSettingsFile() {
-		return $this->getPluginPath() . '/settings.xml';
-	}
-
-	/**
-	 * Get the block context. Overrides parent so that the plugin will be
-	 * displayed during install.
-	 * @return int
-	 */
-	function getBlockContext() {
-		if (!Config::getVar('general', 'installed')) return BLOCK_CONTEXT_LEFT_SIDEBAR;
-		return parent::getBlockContext();
-	}
-
-	/**
-	 * Determine the plugin sequence. Overrides parent so that
-	 * the plugin will be displayed during install.
-	 */
-	function getSeq() {
-		if (!Config::getVar('general', 'installed')) return 1;
-		return parent::getSeq();
-	}
-
-	/**
 	 * Get the display name of this plugin.
 	 * @return String
 	 */
